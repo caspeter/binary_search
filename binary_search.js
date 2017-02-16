a6c340a7b01f0e349944d3e0007da3a7654e27e4
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-16 11:39:02 -0700

    write code for binary search

---
+++ binary_search.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function binarySearch(arr, target){
+  var low = 1;
+  var high = arr.length;
+  while (low <= high) {
+    let middle = Math.floor(low + (high-low)/2);
+    if (arr[middle] === target) {
+      return middle;
+    } else if (arr[middle] < target) {
+      low = middle + 1;
+    } else{
+      high = middle -1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch([2,4,7,9,10,13,16,34,67,97], 4)); //1
+console.log(binarySearch([2,4,7,9,10,13,16,34,67,97], 67)); //8
+console.log(binarySearch([2,4,7,9,10,13,16,34,67,97], 100)); //-1
