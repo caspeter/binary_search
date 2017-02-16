c3e38f7b0dd070449117b000cdcdb8222e33fdf7
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-16 11:40:09 -0700

    add note to Teddi

--- binary_search.js
+++ binary_search.js
@@ -19,3 +19,5 @@ function binarySearch(arr, target){
 console.log(binarySearch([2,4,7,9,10,13,16,34,67,97], 4)); //1
 console.log(binarySearch([2,4,7,9,10,13,16,34,67,97], 67)); //8
 console.log(binarySearch([2,4,7,9,10,13,16,34,67,97], 100)); //-1
+
+// Hi Teddi! You said that if I could write the code before lunch started it would be able to count for my binary search in under 20 minutes. Here it is! Thanks!
